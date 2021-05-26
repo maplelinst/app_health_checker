2bc9405ebbf643fc1d7de993541184aac087dc74
Terrence_Lin <maplelinst@gmail.com>
2021-05-26 22:54:39 +0800

    Init Springboot project

---
+++ src/test/java/org/tlin/healthchecker/HealthcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tlin.healthchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
